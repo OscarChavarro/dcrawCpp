028a40edf833dceaf158bf6e6745565366e46371
Oscar Chavarro <jedilink@gmail.com>
2023-07-01 21:42:49 +0200

    formatting: options class review

--- src/common/Options.cpp
+++ src/common/Options.cpp
@@ -124,21 +124,21 @@ Options::printHelp(const char **argv) {
 int
 Options::setArguments(int argc, const char **argv) {
     int arg;
-    int i;
     char *cp;
     char opm;
     char opt;
     const char *sp;
 
-    if (argc == 1) {
-        OPTIONS_values->printHelp(argv);
+    if ( argc == 1 ) {
+        printHelp(argv);
         exit(1);
     }
     argv[argc] = "";
+
     for ( arg = 1; (((opm = argv[arg][0]) - 2) | 2) == '+'; ) {
         opt = argv[arg++][1];
         if ( (cp = (char *) strchr (sp="nbrkStqmHACg", opt)) ) {
-            for ( i = 0; i < "114111111422"[cp - sp] - '0'; i++ ) {
+            for ( int i = 0; i < "114111111422"[cp - sp] - '0'; i++ ) {
                 if ( !isdigit(argv[arg + i][0])) {
                     fprintf(stderr, "Non-numeric argument to \"-%c\"\n", opt);
                     return 1;
@@ -146,13 +146,12 @@ Options::setArguments(int argc, const char **argv) {
             }
         }
 
-        char *end;
-        switch (opt) {
+        switch ( opt ) {
             case 'n':
-                OPTIONS_values->threshold = atof(argv[arg++]);
+                threshold = atof(argv[arg++]);
                 break;
             case 'b':
-                OPTIONS_values->brightness = atof(argv[arg++]);
+                brightness = atof(argv[arg++]);
                 break;
             case 'r':
                 for ( int c = 0; c < 4; c++ ) {
@@ -160,113 +159,113 @@ Options::setArguments(int argc, const char **argv) {
                 }
                 break;
             case 'C':
-                OPTIONS_values->chromaticAberrationCorrection[0] = 1 / atof(argv[arg++]);
-                OPTIONS_values->chromaticAberrationCorrection[2] = 1 / atof(argv[arg++]);
+                chromaticAberrationCorrection[0] = 1 / atof(argv[arg++]);
+                chromaticAberrationCorrection[2] = 1 / atof(argv[arg++]);
                 break;
             case 'g':
-                OPTIONS_values->gammaParameters[0] = atof(argv[arg++]);
-                OPTIONS_values->gammaParameters[1] = atof(argv[arg++]);
-                if ( OPTIONS_values->gammaParameters[0] ) {
-                    OPTIONS_values->gammaParameters[0] = 1 / OPTIONS_values->gammaParameters[0];
+                gammaParameters[0] = atof(argv[arg++]);
+                gammaParameters[1] = atof(argv[arg++]);
+                if ( gammaParameters[0] ) {
+                    gammaParameters[0] = 1 / gammaParameters[0];
                 }
                 break;
             case 'k':
-                OPTIONS_values->user_black = atoi(argv[arg++]);
+                user_black = atoi(argv[arg++]);
                 break;
             case 'S':
-                OPTIONS_values->user_sat = atoi(argv[arg++]);
+                user_sat = atoi(argv[arg++]);
                 break;
             case 't':
-                OPTIONS_values->user_flip = atoi(argv[arg++]);
+                user_flip = atoi(argv[arg++]);
                 break;
             case 'q':
-                OPTIONS_values->user_qual = atoi(argv[arg++]);
+                user_qual = atoi(argv[arg++]);
                 break;
             case 'm':
-                OPTIONS_values->med_passes = atoi(argv[arg++]);
+                med_passes = atoi(argv[arg++]);
                 break;
             case 'H':
-                OPTIONS_values->highlight = atoi(argv[arg++]);
+                highlight = atoi(argv[arg++]);
                 break;
             case 's':
-                OPTIONS_values->shotSelect = abs(atoi(argv[arg]));
-                OPTIONS_values->multiOut = !strcmp(argv[arg++], "all");
+                shotSelect = abs(atoi(argv[arg]));
+                multiOut = !strcmp(argv[arg++], "all");
                 break;
             case 'o':
                 if ( isdigit(argv[arg][0]) && !argv[arg][1] ) {
-                    OPTIONS_values->outputColorSpace = atoi(argv[arg++]);
+                    outputColorSpace = atoi(argv[arg++]);
                 }
-#ifndef NO_LCMS
+            #ifndef NO_LCMS
                 else {
-                    OPTIONS_values->customOutputProfileForColorSpace = argv[arg++];
+                    customOutputProfileForColorSpace = argv[arg++];
                 }
                 break;
             case 'p':
-                OPTIONS_values->cameraIccProfileFilename = argv[arg++];
-#endif
+                cameraIccProfileFilename = argv[arg++];
+            #endif
                 break;
             case 'P':
-                OPTIONS_values->bpfile = argv[arg++];
+                bpfile = argv[arg++];
                 break;
             case 'K':
-                OPTIONS_values->dark_frame = argv[arg++];
+                dark_frame = argv[arg++];
                 break;
             case 'z':
-                OPTIONS_values->timestamp_only = 1;
+                timestamp_only = 1;
                 break;
             case 'e':
-                OPTIONS_values->thumbnail_only = 1;
+                thumbnail_only = 1;
                 break;
             case 'i':
-                OPTIONS_values->identify_only = 1;
+                identify_only = 1;
                 break;
             case 'c':
-                OPTIONS_values->write_to_stdout = 1;
+                write_to_stdout = 1;
                 break;
             case 'v':
-                OPTIONS_values->verbose = 1;
+                verbose = 1;
                 break;
             case 'h':
-                OPTIONS_values->halfSizePreInterpolation = 1;
+                halfSizePreInterpolation = 1;
                 break;
             case 'f':
-                OPTIONS_values->fourColorRgb = 1;
+                fourColorRgb = 1;
                 break;
             case 'A':
                 for ( int c = 0; c < 4; c++ ) {
-                    OPTIONS_values->greyBox[c] = atoi(argv[arg++]);
+                    greyBox[c] = atoi(argv[arg++]);
                 }
             case 'a':
-                OPTIONS_values->useAutoWb = 1;
+                useAutoWb = 1;
                 break;
             case 'w':
-                OPTIONS_values->useCameraWb = 1;
+                useCameraWb = 1;
                 break;
             case 'M':
-                OPTIONS_values->useCameraMatrix = 3 * (opm == '+');
+                useCameraMatrix = 3 * (opm == '+');
                 break;
             case 'I':
-                OPTIONS_values->readFromStdin = 1;
+                readFromStdin = 1;
                 break;
             case 'E':
-                OPTIONS_values->documentMode++;
+                documentMode++;
             case 'D':
-                OPTIONS_values->documentMode++;
+                documentMode++;
             case 'd':
-                OPTIONS_values->documentMode++;
+                documentMode++;
             case 'j':
-                OPTIONS_values->useFujiRotate = 0;
+                useFujiRotate = 0;
                 break;
             case 'W':
-                OPTIONS_values->noAutoBright = 1;
+                noAutoBright = 1;
                 break;
             case 'T':
-                OPTIONS_values->outputTiff = 1;
+                outputTiff = 1;
                 break;
             case '4':
-                OPTIONS_values->gammaParameters[0] = OPTIONS_values->gammaParameters[1] = OPTIONS_values->noAutoBright = 1;
+                gammaParameters[0] = gammaParameters[1] = noAutoBright = 1;
             case '6':
-                OPTIONS_values->outputBitsPerPixel = 16;
+                outputBitsPerPixel = 16;
                 break;
             default:
                 fprintf (stderr, "Unknown option \"-%c\".\n", opt);
@@ -274,11 +273,10 @@ Options::setArguments(int argc, const char **argv) {
         }
     }
 
-    if (arg == argc) {
+    if ( arg == argc ) {
         fprintf (stderr, "No files to process.\n");
         exit(1);
     }
 
     return arg;
 }
-
