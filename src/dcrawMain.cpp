38c6021e0fec8cb0d9e45f4f4e6f81a858ebbcae
Oscar Chavarro <jedilink@gmail.com>
2023-07-27 22:44:40 +0200

    refactor: Mamiya leaf raw loaders extracted to modules

--- src/dcrawMain.cpp
+++ src/dcrawMain.cpp
@@ -119,6 +119,7 @@ typedef unsigned long long UINT64;
 #include "persistence/readers/rawloaders/pentaxRawLoaders.h"
 #include "persistence/readers/rawloaders/rolleiRawLoaders.h"
 #include "persistence/readers/rawloaders/phaseoneRawLoaders.h"
+#include "persistence/readers/rawloaders/leafRawLoaders.h"
 
 char *meta_data;
 char xtrans[6][6];
@@ -772,46 +773,6 @@ phase_one_correct() {
             }
         }
         free(yval[0]);
-    }
-}
-
-void
-leaf_hdr_load_raw() {
-    unsigned short *pixel = 0;
-    unsigned tile = 0;
-    unsigned r;
-    unsigned c;
-    unsigned row;
-    unsigned col;
-
-    if ( !IMAGE_filters ) {
-        pixel = (unsigned short *) calloc(THE_image.width, sizeof *pixel);
-        memoryError(pixel, "leaf_hdr_load_raw()");
-    }
-    for ( c = 0; c < tiff_samples; c++ ) {
-        for ( r = 0; r < THE_image.height; r++ ) {
-            if ( r % tile_length == 0 ) {
-                fseek(GLOBAL_IO_ifp, GLOBAL_IO_profileOffset + 4 * tile++, SEEK_SET);
-                fseek(GLOBAL_IO_ifp, read4bytes(), SEEK_SET);
-            }
-            if ( IMAGE_filters && c != OPTIONS_values->shotSelect ) {
-                continue;
-            }
-            if ( IMAGE_filters ) {
-                pixel = THE_image.rawData + r * THE_image.width;
-            }
-            readShorts(pixel, THE_image.width);
-            if ( !IMAGE_filters && (row = r - top_margin) < height ) {
-                for ( col = 0; col < width; col++ ) {
-                    GLOBAL_image[row * width + col][c] = pixel[col + left_margin];
-                }
-            }
-        }
-    }
-    if ( !IMAGE_filters ) {
-        ADOBE_maximum = 0xffff;
-        GLOBAL_colorTransformForRaw = 1;
-        free(pixel);
     }
 }
 
---
+++ src/persistence/readers/rawloaders/leafRawLoaders.h
@@ -0,0 +1,6 @@
+#ifndef __LEAFRAWLOADERS__
+#define __LEAFRAWLOADERS__
+
+extern void leaf_hdr_load_raw();
+
+#endif
