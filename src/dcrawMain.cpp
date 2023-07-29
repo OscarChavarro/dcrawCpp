bcbda6a3780a216af79f52656b522d2b4c61125e
Oscar Chavarro <jedilink@gmail.com>
2023-07-29 15:15:05 +0200

    refactor: Panasonic raw loaders extracted to modules

--- src/dcrawMain.cpp
+++ src/dcrawMain.cpp
@@ -117,6 +117,7 @@ typedef unsigned long long UINT64;
 #include "persistence/readers/rawloaders/sinarRawLoaders.h"
 #include "persistence/readers/rawloaders/imaconRawLoaders.h"
 #include "persistence/readers/rawloaders/nokiaRawLoaders.h"
+#include "persistence/readers/rawloaders/panasonicRawLoaders.h"
 
 char *meta_data;
 char xtrans[6][6];
@@ -312,61 +313,6 @@ layer_thumb() {
         }
     }
     free(thumb);
-}
-
-unsigned
-pana_bits(int nbits) {
-    static unsigned char buf[0x4000];
-    static int vbits;
-    int byte;
-
-    if ( !nbits ) {
-        return vbits = 0;
-    }
-    if ( !vbits ) {
-        fread(buf + GLOBAL_loadFlags, 1, 0x4000 - GLOBAL_loadFlags, GLOBAL_IO_ifp);
-        fread(buf, 1, GLOBAL_loadFlags, GLOBAL_IO_ifp);
-    }
-    vbits = (vbits - nbits) & 0x1ffff;
-    byte = vbits >> 3 ^ 0x3ff0;
-    return (buf[byte] | buf[byte + 1] << 8) >> (vbits & 7) & ~(-1 << nbits);
-}
-
-void
-panasonic_load_raw() {
-    int row;
-    int col;
-    int i;
-    int j;
-    int sh = 0;
-    int pred[2];
-    int nonz[2];
-
-    pana_bits(0);
-    for ( row = 0; row < height; row++ ) {
-        for ( col = 0; col < THE_image.width; col++ ) {
-            if ( (i = col % 14) == 0 ) {
-                pred[0] = pred[1] = nonz[0] = nonz[1] = 0;
-            }
-            if ( i % 3 == 2 ) {
-                sh = 4 >> (3 - pana_bits(2));
-            }
-            if ( nonz[i & 1] ) {
-                if ( (j = pana_bits(8)) ) {
-                    if ((pred[i & 1] -= 0x80 << sh) < 0 || sh == 4 )
-                        pred[i & 1] &= ~(-1 << sh);
-                    pred[i & 1] += j << sh;
-                }
-            } else {
-                if ( (nonz[i & 1] = pana_bits(8)) || i > 11 ) {
-                    pred[i & 1] = nonz[i & 1] << 4 | pana_bits(4);
-                }
-            }
-            if ( (RAW(row, col) = pred[col & 1]) > 4098 && col < width ) {
-                inputOutputError();
-            }
-        }
-    }
 }
 
 void
---
+++ src/persistence/readers/rawloaders/panasonicRawLoaders.h
@@ -0,0 +1,6 @@
+#ifndef DCRAW_PANASONICRAWLOADERS_H
+#define DCRAW_PANASONICRAWLOADERS_H
+
+extern void panasonic_load_raw();
+
+#endif
